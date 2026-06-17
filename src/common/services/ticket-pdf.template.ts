20222af5921fd363168085bc7a80098228c35d44
0xMCoin <0xm.coin@gmail.com>
2026-06-16 23:10:24 -0300

    fix: id da inscricao pdf

--- src/common/services/ticket-pdf.template.ts
+++ src/common/services/ticket-pdf.template.ts
@@ -750,7 +750,7 @@ export const TicketPdfDocument = ({ data }: { data: TicketPdfTemplateData }) => 
             React.createElement(
               Text,
               { style: { fontFamily: 'DM Sans', fontSize: 11, fontWeight: 400, color: C.gray12 } },
-              'ID do ingresso: ',
+              'ID da inscrição: ',
             ),
             React.createElement(
               Text,
